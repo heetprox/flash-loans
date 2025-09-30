7a1ecaf7e5a77f01a3ff62208c246862e146bc0c
virtualheet <heetvavadiya456@gmail.com>
2025-09-30 23:25:32 +0530

    refactor(V4Pool): remove static pool initialization in favor of dynamic
    The static pool initialization was removed to allow for dynamic pool creation, which provides more flexibility in pool management.

--- src/V4Pool.sol
+++ src/V4Pool.sol
@@ -1,11 +1,5 @@
 import {PoolKey} from "v4-core/types/PoolKey.sol";
 
 contract V4Pool {
-    PoolKey pool = PoolKey({
-        currency0: currency0,
-        currency1: currency1,
-        fee: lpFee,
-        tickSpacing: tickSpacing,
-        hooks: hookContract
-    });
+    // dynamic-pool init
 }
